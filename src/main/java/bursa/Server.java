8556b5203e096cc8348b5e898173531392f14efb
DumitrescuBogdanDoru <bogdandoru.dumitrescu@gmail.com>
2021-11-09 17:21:03 +0200

    M-2: empty project created

---
+++ src/main/java/bursa/Server.java
@@ -0,0 +1,13 @@
+package bursa;
+
+/**
+ * Created by: Doru
+ * Date: 09/11/2021
+ */
+
+public class Server {
+
+    public static void main(String[] args) {
+        System.out.println("Server is running");
+    }
+}
