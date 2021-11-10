c480ba0ec20e16ff6929e21ab8aca38b2d77d75b
Cristian Dragoescu <cristian.drgg@gmail.com>
2021-11-10 17:19:27 +0200

    M-2: added Stock

---
+++ src/main/java/bursa/Stock.java
@@ -0,0 +1,38 @@
+package models;
+
+public class Stock {
+
+    private Integer id;
+    private String name;
+    private float price;
+
+    public Stock(Integer id, String name, float price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
