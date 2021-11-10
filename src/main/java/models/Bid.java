8b248d69413c56ebed65c0aa883ba9a0a61cc0f8
Cristian Dragoescu <cristian.drgg@gmail.com>
2021-11-10 17:18:04 +0200

    M-2: added Bid

---
+++ src/main/java/models/Bid.java
@@ -0,0 +1,28 @@
+package models;
+
+public class Bid {
+
+    private Integer id;
+    private double price;
+
+    public Bid(Integer id, double price) {
+        this.id = id;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
