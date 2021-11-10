e361b6537f7c10fd09e247604f81c2287a5f4976
DumitrescuBogdanDoru <bogdandoru.dumitrescu@gmail.com>
2021-11-10 17:37:32 +0200

    M-2: modified Server

--- src/main/java/bursa/Server.java
+++ src/main/java/bursa/Server.java
@@ -1,5 +1,8 @@
 package bursa;
 
+import threads.BuyerThread;
+import threads.SellerThread;
+
 /**
  * Created by: Doru
  * Date: 09/11/2021
@@ -8,6 +11,10 @@ package bursa;
 public class Server {
 
     public static void main(String[] args) {
-        System.out.println("Server is running");
+        System.out.println("This is a server");
+        SellerThread sellerThread = new SellerThread();
+        sellerThread.start();
+        BuyerThread buyerThread = new BuyerThread();
+        buyerThread.start();
     }
 }
